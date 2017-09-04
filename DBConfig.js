61d210aab39182f4ac365f4a221f797b77a91ab7
wednezday <wednezdayy@gmail.com>
2017-09-04 15:00:56 +0700

    chok

--- DBConfig.js
+++ DBConfig.js
@@ -2,15 +2,15 @@ module.exports = {
   ArnomaDB: {
     user: 'sa',
     password: 'P@ssw0rd',
-    server: 'MSC23832',
-    //server: '127.0.0.1\\mssql2008r2',
+    // server: 'MSC23832',
+    server: '192.168.28.125\\mssql2008r2',
     database: 'ARNOMA_HMS_DB_LIVE'
   },
   EmailDB: {
     user: 'sa',
     password: 'P@ssw0rd',
-    server: 'MSC23832',
-    //server: '127.0.0.1\\mssql2008r2',
+    // server: 'MSC23832',
+    server: '192.168.28.125\\mssql2008r2',
     database: 'EmailDB'
   }
 };
