ee8760fc3af110226c952aa1ed29f705704ba6fa
msc23832 <sivaccha@metrosystems.co.th>
2017-09-04 11:17:36 +0700

    UPLOAD express master

--- DBConfig.js
+++ DBConfig.js
@@ -2,14 +2,14 @@ module.exports = {
   ArnomaDB: {
     user: 'sa',
     password: 'P@ssw0rd',
-    server: '192.168.1.100\\MSC23832',
+    server: 'MSC23832',
     //server: '127.0.0.1\\mssql2008r2',
     database: 'ARNOMA_HMS_DB_LIVE'
   },
   EmailDB: {
     user: 'sa',
     password: 'P@ssw0rd',
-    server: '192.168.1.100\\MSC23832',
+    server: 'MSC23832',
     //server: '127.0.0.1\\mssql2008r2',
     database: 'EmailDB'
   }
